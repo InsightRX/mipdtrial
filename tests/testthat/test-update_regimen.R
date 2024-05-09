871296f41a9357616758f3faaa773c775f51ddc6
jasmineirx <jasmine@insight-rx.com>
2024-05-08 20:23:00 -0400

    fix tests

--- tests/testthat/test-update_regimen.R
+++ tests/testthat/test-update_regimen.R
@@ -8,7 +8,7 @@ test_that("Update regimen updates correctly", {
   new_reg <- update_regimen(original, 3, 4)
   expect_true(inherits(new_reg, "regimen"))
   expect_equal(new_reg$dose_amts, c(1, 1, 1, 3, 3))
-  constant_cols <- setdiff(names(regimen), "dose_amts")
+  constant_cols <- setdiff(names(original), "dose_amts")
   expect_true(
     all(unlist(lapply(constant_cols, \(x) all(new_reg[[x]] == original[[x]]))))
   )
