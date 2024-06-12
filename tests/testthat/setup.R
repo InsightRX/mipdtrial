949ece9689aacbabec145a1a02bba7d19cc9a8e4
jasmineirx <jasmine@insight-rx.com>
2024-06-12 14:16:59 -0400

    update library path for windows CI

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,8 @@
 mod_1cmt_iv <- PKPDsim::new_ode_model("pk_1cmt_iv_auc")
 suppressMessages({ ## avoid message "the following objects are masked from ..."
+  if (.Platform$OS.type == "windows") {
+    .libPaths <- c("..", .libPaths())
+  }
   if (!require("pkbusulfanmccune", character.only = TRUE)) {
     PKPDsim::install_default_literature_model("pk_busulfan_mccune")
     library(pkbusulfanmccune)
