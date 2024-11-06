4ec544d5bc73298bcff746484f23f046bce4f0d9
jasmineirx <jasmine@insight-rx.com>
2024-11-06 18:04:03 -0500

    add example

--- R/create_initial_regimen_design.R
+++ R/create_initial_regimen_design.R
@@ -2,8 +2,23 @@
 #'
 #' @param method a function to call. The function should always return a
 #' PKPDsim regimen object.
+#' @param ... options passed on depending on `method`. Commonly includes
+#' `regimen`, a named list of arguments passed on to PKPDsim::new_regimen
+#' (see examples)
 #'
 #' @returns a list object with the design specs
+#' @examples
+#' # create_initial_regimen_design(
+#'   method = weight_based_starting_dose,
+#'   mgkg = 15,
+#'   regimen = list(
+#'     interval = 12,
+#'     n = 10,
+#'     t_inf = 1,
+#'     type = "infusion"
+#'   )
+#' )
+#'
 #' @export
 #'
 create_initial_regimen_design <- function(
