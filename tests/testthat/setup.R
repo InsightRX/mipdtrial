f3f526d93ef59dc43667e418a30fe5d2df5de080
jasmineirx <jasmine@insight-rx.com>
2024-11-09 15:11:20 -0500

    address CI warnings

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,6 +5,6 @@ suppressMessages({ ## avoid message "the following objects are masked from ..."
   }
   if (!require("pkbusulfanmccune", character.only = TRUE)) {
     PKPDsim::install_default_literature_model("pk_busulfan_mccune")
-    library(pkbusulfanmccune)
+    loadNamespace(pkbusulfanmccune)
   }
 })
