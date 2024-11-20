dd2b1a5ba3088ef200c249ec6df32aac43e6aa83
roninsightrx <ron@insight-rx.com>
2024-11-20 10:54:53 -0800

    Update R/exposure_metrics.R
    Co-authored-by: Jasmine Hughes <43552465+jasmineirx@users.noreply.github.com>

--- R/exposure_metrics.R
+++ R/exposure_metrics.R
@@ -110,8 +110,7 @@ calc_auc_from_regimen <- function(
   )
   if(target_design$type == "auc24") {
     target_time <- c(target_time - 24, target_time)
-  }
-  if(target_design$type == "auc12") {
+  } else if (target_design$type == "auc12") {
     target_time <- c(target_time - 12, target_time)
   }
   sim_output <- PKPDsim::sim(
